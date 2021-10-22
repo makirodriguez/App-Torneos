6c3457b1815dfc8c6dc85f8d5d8e5b9baad1d4f2
Julian Noceloni <julian_noceloni@hotmail.com>
2021-10-22 18:08:13 -0300

    Cambio logo y agregue pagina Perfil

--- amplify-react-app/src/pages/Home/Home.jsx
+++ amplify-react-app/src/pages/Home/Home.jsx
@@ -4,9 +4,10 @@ const Home = () => {
     <div class="px-4 py-5 my-5 text-center">
         <h1 class="display-5 fw-bold">Bienvenido/a!</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+          <p class="lead mb-4">Acá vas a poder crear torneos, contando con todas las herramientas necesarias
+          para llevar el seguimiento del mismo al día. </p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-dark btn-lg px-4 gap-3">Crear torneo</button>
+            <button type="button" class="btn btn-light btn-lg px-4 gap-3">Crear torneo</button>
           </div>
         </div>
       </div>
---
+++ amplify-react-app/src/pages/Perfil/Perfil.jsx
@@ -0,0 +1,40 @@
+const Perfil = () => {
+    return (
+        <div class="container rounded bg-white mt-5 mb-5">
+            <div class="row">
+                <div class="col-md-5 border-right">
+                        <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                            <img class="rounded-circle mt-5" src=""></img>
+                            <div class="mb-3">
+                                <label for="formFile" class="form-label">Cambiar imagen de perfil</label>
+                                <input class="form-control" type="file" id="formFile"></input>
+                            </div>
+                            <span class="font-weight-bold">Nombre</span><span class="text-black-50">Mail</span><span></span>
+                        </div>
+                </div>
+                <div class="col-md-5 border-right">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h4 class="text-right">Configurar perfil</h4>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-md-6"><label class="labels">Nombre</label><input type="text" class="form-control" placeholder="Nombre" value=""></input></div>
+                            <div class="col-md-6"><label class="labels">Apellido</label><input type="text" class="form-control" value="" placeholder="Apellido"></input></div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-12"><label class="labels">Numero de telefono</label><input type="text" class="form-control" placeholder="Ingresar numero de telefono" value=""></input></div>
+                            <div class="col-md-12"><label class="labels">Email</label><input type="email" class="form-control" placeholder="usuario@ejemplo.com" value=""></input></div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6"><label class="labels">País</label><input type="text" class="form-control" placeholder="Ingresar país" value=""></input></div>
+                            <div class="col-md-6"><label class="labels">Provincia</label><input type="text" class="form-control" value="" placeholder="Ingresar provincia"></input></div>
+                        </div>
+                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Guardar cambios</button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+};
+
+export default Perfil;
