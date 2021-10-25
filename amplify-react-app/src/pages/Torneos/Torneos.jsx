6e902696f5b75ce354fcd41b0c70c158a7ec97aa
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2021-10-25 12:16:42 -0300

    Creo formulario que guarde los datos cargados en Dynamo

--- amplify-react-app/src/pages/Torneos/Torneos.jsx
+++ amplify-react-app/src/pages/Torneos/Torneos.jsx
@@ -1,131 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import {Amplify, API, graphqlOperation} from 'aws-amplify';
-import { listTorneos } from '../../graphql/queries';
-import awsconfig from '../../aws-exports';
-import { createTorneo } from '../../graphql/mutations';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect, Fragment } from 'react';
+import {API} from 'aws-amplify';
+import * as queries from '../../graphql/queries';
+import * as mutations from '../../graphql/mutations';
 
 
+const Torneo = () => {
 
-Amplify.configure(awsconfig);
+  const [torneos, setTorneos] = useState({
+    name: '',
+    sport: '',
+    startDate: '',
+    endDate: '',
+    description: ''
+  });
 
-const Torneo = ({ onUpload }) =>{
-  const [torneos, setTorneos] = useState({});
-  
-/* 
+  const [listTorneos, setListTorneos] = useState([]);
   useEffect(() =>{
-    fetchTorneos();
-  }, []);  */
-
-  const fetchTorneos = async () => {
-
-    try{
-      console.log('torneos', torneos);
-      const {name, sport, startDate, endDate, description} = torneos;
-      const createTorneoInput = {
-        name,
-        sport,
-        startDate,
-        endDate,
-        description
-      };
-
-    console.log(createTorneoInput);
-    
-      await API.graphql(graphqlOperation(createTorneo, { input: createTorneoInput }));
-      onUpload();
-      //const torneosData = await API.graphql(graphqlOperation(listTorneos ));
-    /*   const TorneoList= torneosData.data.listTorneos.items;
-      console.log('TorneoList', TorneoList);
-      setTorneos(TorneoList); */
-    }
-    catch (err) {console.log('Error fetching torneos: ', err)}
-  };
+      async function getAllTorneos(){
+        const allTorneos = await API.graphql({query: queries.listTorneos});
+        setListTorneos(allTorneos.data.listTorneos.items);  
+      }
+    getAllTorneos();
+  }); 
 
 
-  return (
-    <div>
-          <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1 },
-      }}
-      noValidate
-      autoComplete="off"
+  const handleFormSubmit = async (e)  =>{
 
-    >
-    <h1>Nuevo torneo</h1>
-
-      <FormControl variant="standard">
-        <InputLabel htmlFor="component-helper">Nombre del torneo</InputLabel>
-        <Input
-          id="component-helper"
-          value={torneos.name}
-          onChange={e => setTorneos({ ...torneos, name: e.target.value })}
-          aria-describedby="component-helper-text"
-        />
-        <FormHelperText id="component-helper-text">
-          Campo obligatorio
-        </FormHelperText>
-        </FormControl>
-        <FormControl variant="standard">
-        <InputLabel htmlFor="component-helper">Nombre del deporte</InputLabel>
-        <Input
-          id="component-helper"
-          value={torneos.sport}
-          onChange={e => setTorneos({ ...torneos, sport: e.target.value })}
-          aria-describedby="component-helper-text"
-        />
-        <FormHelperText id="component-helper-text">
-          Campo obligatorio
-        </FormHelperText>
-        </FormControl>
-      <FormControl variant="standard">
-        <InputLabel htmlFor="component-helper">Fecha de inicio</InputLabel>
-        <Input
-          id="component-helper"
-          type="date"
-          value={torneos.startDate}
-          onChange={e => setTorneos({ ...torneos, startDate: e.target.value })}
-          aria-describedby="component-helper-text"
-        />
-        </FormControl>
-        <FormControl variant="standard">
-        <InputLabel htmlFor="component-helper">Fecha de fin</InputLabel>
-        <Input
-          id="component-helper"
-          type="date"
-          value={torneos.endDate}
-          onChange={e => setTorneos({ ...torneos, endDate: e.target.value })}
-          aria-describedby="component-helper-text"
-        />
-        </FormControl>
-        <FormControl variant="standard">
-        <InputLabel htmlFor="component-helper">Descripcion</InputLabel>
-        <Input
-          id="component-helper"
-          value={torneos.description}
-          onChange={e => setTorneos({ ...torneos, description: e.target.value })}
-          aria-describedby="component-helper-text"
-        />
-        <FormHelperText id="component-helper-text">
-          Campo obligatorio
-        </FormHelperText>
-        </FormControl>
-  
-      <input type="submit" onClick={fetchTorneos} value="Crear torneo"></input>
+     const createTorneoInput = {
+      name: torneos.name,
+      sport: torneos.sport,
+      startDate: torneos.startDate,
+      endDate: torneos.endDate,
+      description: torneos.description
+    } 
+    await API.graphql({query: mutations.createTorneo, variables: {input: createTorneoInput}});
+  }
 
 
-    </Box>
-    </div>
-  ); }
+  const handleInputChange = (e) =>{
+    setTorneos({...torneos, [e.target.name]: e.target.value})
+  }
 
-  export default Torneo;
+  return(
+    <Fragment>
+        <div>
+
+          <h1>Nuevo torneo</h1>
+          <form className="column" onSubmit={handleFormSubmit}>
+            <div className="col-md-2">
+              <input  className="form-control"
+              placeholder="Ingrese el nombre del torneo"  
+              type="text" 
+              name="name" 
+              onChange={handleInputChange}/>
+            </div>
+            <div className="col-md-2">
+              <input className="form-control"
+              placeholder="Ingrese el deporte" 
+              type="text" 
+              name="sport" 
+              onChange={handleInputChange}/> 
+            </div>
+            <div className="col-md-2">
+              <input className="form-control" 
+              type="date" 
+              name="startDate" 
+              onChange={handleInputChange}/> 
+            </div>
+            <div className="col-md-2">
+              <input className="form-control" 
+              type="date" 
+              name="endDate" 
+              onChange={handleInputChange}/> 
+            </div>
+            <div className="col-md-2">
+              <input className="form-control"
+              placeholder="Ingrese la descripcion"
+              type="text" 
+              name="description" 
+              onChange={handleInputChange}/> 
+            </div>
+            <div className="col-md-2">
+              <button className="btn btn-dark">Crear torneo</button>
+            </div>
+          </form>
+        </div>
+        <div>    
+          <p> Torneo: {torneos.name}- {torneos.sport}</p>
+          {listTorneos && listTorneos.map(item =>
+            <li key={item.id}>
+              {item.name}
+              {item. sport}
+            </li>
+          )}
+        </div>
+    </Fragment>
+  );
+} 
+     
+export default Torneo;
     
 
 
