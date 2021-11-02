e5cce5c1f6d56e9fddbef1440265e2e15dce5d51
Julian Noceloni <julian_noceloni@hotmail.com>
2021-11-02 20:06:28 -0300

    Cambios de diseño en pestaña Mis Torneos

--- amplify-react-app/src/pages/Mis torneos/Mis torneos.jsx
+++ amplify-react-app/src/pages/Mis torneos/Mis torneos.jsx
@@ -15,24 +15,29 @@ const MisTorneos = () => {
 
     return (
         <Fragment>
-            <div class="container rounded bg-dark mt-5 mb-5">
-                <h1>Mis torneos</h1>
+            <div class="container rounded bg-dark mt-5 mb-5"><br></br>
+                <div class="container-fluid col-md-4 rounded bg-white mb-5">
+                    <div class="row">
+                        <h1 class ="display-1">Mis torneos</h1>
+                    </div>
+                </div>
                     {listTorneos && listTorneos.map(item =>
-                        <div class="container rounded bg-white mt-4 mb-5">
-                            <li key={item.id}>
-                                <h2>Nombre del torneo</h2>
-                                {item.name} <br></br>
-                                <h3>Deporte</h3>
-                                {item.sport} <br></br>
-                                <h3>Fecha de inicio</h3>
-                                {item.startDate} <br></br>
-                                <h3>Fecha de finalización</h3>
-                                {item.endDate} <br></br>
-                                <h3>Descripción</h3>
-                                {item.description} <br></br>
-                            </li>
+                        <div class="container-fluid col-md-5 rounded bg-white mt-4 mb-5">
+                            <div class="row">
+                                <p key={item.id}>
+                                    <h1>Nombre del torneo</h1>
+                                    <h2>{item.name}</h2><br></br>
+                                    <h1>Deporte</h1>
+                                    <h2>{item.sport}</h2><br></br>
+                                    <h1>Fecha de inicio: {item.startDate}</h1><br></br>
+                                    <h1>Fecha de fin: {item.endDate}</h1><br></br>
+                                    <h1>Descripción: {item.description}</h1><br></br><br></br>
+                                    <button className="btn btn-primary btn-lg">Modificar</button> <button className="btn btn-danger btn-lg">Eliminar</button>
+                                </p>
+                            </div>
                         </div>
                     )}
+                <br></br>
             </div>
         </Fragment>
     );
